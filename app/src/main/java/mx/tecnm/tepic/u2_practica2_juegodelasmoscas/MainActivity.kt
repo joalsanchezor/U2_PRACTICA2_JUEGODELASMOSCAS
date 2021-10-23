ecaee1ba82d671937c5f4ceb883c7d62f6c9422e
[joalsanchezor] <[joalsanchezor@ittepic.edu.mx]>
2021-10-22 19:27:00 -0600

    Initial commit

---
+++ app/src/main/java/mx/tecnm/tepic/u2_practica2_juegodelasmoscas/MainActivity.kt
@@ -0,0 +1,11 @@
+package mx.tecnm.tepic.u2_practica2_juegodelasmoscas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
